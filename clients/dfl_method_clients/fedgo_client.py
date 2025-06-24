a647e7dca242b28c0f7f1ade9ac5417a892d64b4
Jiahui Bai <835862228@qq.com>
2025-06-24 11:38:38 +1000

    Improve FedGO memory overhead

--- clients/dfl_method_clients/fedgo_client.py
+++ clients/dfl_method_clients/fedgo_client.py
@@ -42,26 +42,24 @@ class Discriminator(nn.Module):
 class FedGOClient(Client):
     def __init__(self, client_id, dataset_index, full_dataset, hyperparam, device):
         super().__init__(client_id, dataset_index, full_dataset, hyperparam, device)
-        # FedGO 参数
         self.noise_dim = hyperparam.get('noise_dim', 100)
         self.gen_epochs = hyperparam.get('gen_epochs', 3)
         self.disc_epochs = hyperparam.get('disc_epochs', 1)
         self.unlabeled_samples = hyperparam.get('unlabeled_samples', 128)
         self.lambda_kd = hyperparam.get('lambda_kd', 1.0)
         self.lambda_ce = hyperparam.get('lambda_ce', 1.0)
-        # 模型与判别器、生成器
-        self.generator = Generator(self.noise_dim, full_dataset[0][0].shape).to(device)
-        self.discriminator = Discriminator(full_dataset[0][0].shape).to(device)
-        # 邻居参数存储
-        self.peer_models = {}         # id -> state_dict
-        self.peer_discriminators = {} # id -> state_dict
-        self.is_later = False
 
+        input_shape = full_dataset[0][0].shape
+        self.generator = Generator(self.noise_dim, input_shape).to(device)
+        self.discriminator = Discriminator(input_shape).to(device)
+
+        self.peer_models = {}
+        self.peer_discriminators = {}
+        self.is_later = False
 
     def set_init_model(self, model):
         # 初始化全局模型
         self.model = deepcopy(model)
-        # 预训练：generator + discriminator
         self.train_generator()
         self.train_discriminator()
 
@@ -74,13 +72,12 @@ class FedGOClient(Client):
         else:
             self._local_train()
 
-
-
     def send_model(self):
-        # 发送 model 和 discriminator 状态
+        model_cpu = {k: v.cpu() for k, v in self.model.state_dict().items()}
+        disc_cpu = {k: v.cpu() for k, v in self.discriminator.state_dict().items()}
         return (self.id, {
-            'model': deepcopy(self.model.state_dict()),
-            'discriminator': deepcopy(self.discriminator.state_dict())
+            'model': model_cpu,
+            'discriminator': disc_cpu
         })
 
     def receive_neighbor_model(self, neighbor_model):
@@ -90,41 +87,41 @@ class FedGOClient(Client):
         self.peer_discriminators[client_id] = data['discriminator']
 
     def _local_train_kd(self):
-        # FedGO 聚合：蒸馏
-        # 1. 生成伪未标注数据
         self.generator.eval()
         with torch.no_grad():
             z = torch.randn(self.unlabeled_samples, self.noise_dim, device=self.device)
             fake_x = self.generator(z)
-        # 2. 计算伪标签
-        pseudo_labels = []  # tensor list
-        for x in fake_x:
-            x = x.unsqueeze(0)
-            logits_sum = 0
-            weight_sum = 0
-            for cid, mstate in self.peer_models.items():
-                # peer model forward
-                peer = deepcopy(self.model).to(self.device)
-                peer.load_state_dict(mstate)
+
+        with torch.no_grad():
+            logits_sum = torch.zeros(fake_x.size(0), self.num_classes, device=self.device)
+            weight_sum = torch.zeros_like(logits_sum)
+
+            for cid, state_dict in self.peer_models.items():
+                peer = type(self.model)().to(self.device)
+                peer.load_state_dict(state_dict)
                 peer.eval()
-                out = peer(x)
-                # peer discriminator
-                disc = Discriminator(x.shape[1:]).to(self.device)
+
+                disc = Discriminator(fake_x.shape[1:]).to(self.device)
                 disc.load_state_dict(self.peer_discriminators[cid])
-                d_out = torch.sigmoid(disc(x))
+                disc.eval()
+
+                out = peer(fake_x)
+                d_out = torch.sigmoid(disc(fake_x))
                 odds = d_out / (1 - d_out + 1e-6)
+
                 logits_sum += odds * out
                 weight_sum += odds
-            logits_avg = logits_sum / (weight_sum + 1e-6)
-            pseudo_labels.append(logits_avg.squeeze(0))
-        pseudo_labels = torch.stack(pseudo_labels)
-        # 3. 蒸馏训练
-        # 构建 unlabeled DataLoader
+
+                del peer, disc, out, d_out, odds
+                torch.cuda.empty_cache()
+
+            pseudo_labels = logits_sum / (weight_sum + 1e-6)
+
         dataset = TensorDataset(fake_x, pseudo_labels)
         loader_u = DataLoader(dataset, batch_size=self.client_val_loader.batch_size, shuffle=True)
         self.model.train()
+
         for _ in range(self.epochs):
-            # KD on unlabeled
             for x_u, y_u in loader_u:
                 x_u, y_u = x_u.to(self.device), y_u.to(self.device)
                 self.optimizer.zero_grad()
@@ -133,7 +130,7 @@ class FedGOClient(Client):
                 loss_kd *= self.lambda_kd
                 loss_kd.backward()
                 self.optimizer.step()
-            # CE on real data
+
             for x_r, labels in self.client_train_loader:
                 x_r, labels = x_r.to(self.device), labels.to(self.device)
                 self.optimizer.zero_grad()
@@ -141,12 +138,12 @@ class FedGOClient(Client):
                 loss_ce = F.cross_entropy(out_r, labels) * self.lambda_ce
                 loss_ce.backward()
                 self.optimizer.step()
-        # 清空 peer buffers
+
         self.peer_models.clear()
         self.peer_discriminators.clear()
+        torch.cuda.empty_cache()
 
     def train_generator(self):
-        # 本地用真实数据训练生成器
         self.generator.train()
         opt_g = torch.optim.Adam(self.generator.parameters(), lr=1e-3)
         for _ in range(self.gen_epochs):
@@ -159,30 +156,30 @@ class FedGOClient(Client):
                 loss.backward()
                 opt_g.step()
 
-    def aggregate(self):
-        self.model.load_state_dict(self._weight_aggregation())
-        self.neighbor_model_weights.clear()
-
     def train_discriminator(self):
-        # 本地训练判别器分辨真实 vs 生成
         self.discriminator.train()
         opt_d = torch.optim.Adam(self.discriminator.parameters(), lr=1e-3)
         bce = nn.BCEWithLogitsLoss()
         for _ in range(self.disc_epochs):
             for x, _ in self.client_train_loader:
                 x = x.to(self.device)
-                # real
                 real_label = torch.ones(x.size(0), 1, device=self.device)
+                fake_label = torch.zeros(x.size(0), 1, device=self.device)
+
                 out_real = self.discriminator(x)
                 loss_real = bce(out_real, real_label)
-                # fake
+
                 z = torch.randn(x.size(0), self.noise_dim, device=self.device)
                 fake_x = self.generator(z).detach()
-                fake_label = torch.zeros(x.size(0), 1, device=self.device)
                 out_fake = self.discriminator(fake_x)
                 loss_fake = bce(out_fake, fake_label)
-                # update
+
                 loss = (loss_real + loss_fake) * 0.5
                 opt_d.zero_grad()
                 loss.backward()
                 opt_d.step()
+
+    def aggregate(self):
+        self.model.load_state_dict(self._weight_aggregation())
+        self.neighbor_model_weights.clear()
+        torch.cuda.empty_cache()
